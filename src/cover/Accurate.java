1cd357a465f87d190da5cd42bb332926bedeb29b
Szymon Michniak <s.michniak@icloud.com>
2020-05-03 23:30:18 +0200

    Accurate optimalization

--- src/cover/Accurate.java
+++ src/cover/Accurate.java
@@ -11,42 +11,41 @@ public class Accurate extends Algorithm {
     }
 
 
-    private static ArrayList<Integer> coverAux(CollectionToCover toCover, ArrayList<CollectionSet> collectionFamily, int startingIndex) {
-        if (toCover.covered() || startingIndex == collectionFamily.size()) {
+    private ArrayList<Integer> coverAux(CollectionToCover toCover, ArrayList<CollectionSet> collectionFamily, int startingIndex) {
+        if (startingIndex == collectionFamily.size()) {
             return new ArrayList<>();
         }
-        //TODO
-        //If intersection empty, withoutFirst without copying
+        Set<Integer> intersection = collectionFamily.get(startingIndex).intersection(toCover.getNotCovered());
+
+        if (intersection.size() == 0) {
+            return coverAux(toCover, collectionFamily, startingIndex + 1);
+        }
 
         Set<Integer> copy = new HashSet<>(toCover.getNotCovered());
         int max = toCover.getMaximum();
         CollectionToCover toCoverCopy = new CollectionToCover(max, copy);
-        ArrayList<Integer> withoutFirst = coverAux(toCoverCopy, collectionFamily, startingIndex + 1);
 
-        Set<Integer> intersection = collectionFamily.get(startingIndex).intersection(toCover.getNotCovered());
-        ArrayList<Integer> withFirst = new ArrayList<>();
         toCover.cover(intersection);
+        ArrayList<Integer> withFirst = new ArrayList<>();
 
         if (toCover.covered()) {
             withFirst.add(startingIndex + 1);
-        } else if (intersection.size() > 0) {
-            withFirst = coverAux(toCover, collectionFamily, startingIndex + 1);
-            if (withFirst.size() > 0) {
-                withFirst.add(0, startingIndex + 1);
-            }
+            return withFirst;
         }
 
+        withFirst = coverAux(toCover, collectionFamily, startingIndex + 1);
+        ArrayList<Integer> withoutFirst = coverAux(toCoverCopy, collectionFamily, startingIndex + 1);
+
+        if (withFirst.size() == 0) {
+            return withoutFirst;
+        }
+
+        withFirst.add(0, startingIndex + 1);
 
         if (withoutFirst.size() == 0) {
             return withFirst;
         }
-        if (withFirst.size() == 0) {
-            return withoutFirst;
-        }
-        if (withFirst.size() <= withoutFirst.size()) {
-            return withFirst;
-        }
-        return withoutFirst;
-
+        return withFirst.size() <= withoutFirst.size() ? withFirst : withoutFirst;
     }
+
 }
--- src/cover/Main.java
+++ src/cover/Main.java
@@ -1,7 +1,5 @@
 package cover;
 
-import cover.Parser;
-
 public class Main {
     public static void main(String[] args) {
         Parser parser = new Parser();
@@ -9,3 +7,26 @@ public class Main {
 
     }
 }
+
+/*
+2 0
+
+   1      0 1
+
+0 -3
+1
+0
+2 3 2 0
+-3 3
+-3 2
+-3 1
+4 -1 -5 1000000000 0
+2 -2 0
+-6 3
+-6 2
+-6 1
+1 6 0
+-6 3
+-6 2
+-6 1
+ */
